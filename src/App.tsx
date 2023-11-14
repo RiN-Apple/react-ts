145de282ee81c2d78c2c088b347e0e7b781e7caa
Takase Rintaro <tridwn0ek@gmail.com>
2023-11-14 21:20:55 +0900

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className="mx-auto w-1/3">
+      <div className="flex flex-col mt-6">
+        <div className="">React</div>
+      </div>
+    </div>
+  );
+};
