e4516917b6909d81abed3d4f97e06eaedf45849f
Takase Rintaro <tridwn0ek@gmail.com>
2023-11-14 23:32:02 +0900

    [update]: コメント追加とディレクトリ整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
-import { CountUpButton } from "./CountUpButton";
-import { CountInput } from "./CountInput";
-import { CountText } from "./CountText";
+import { CountUpButton } from "./components/CountUpButton";
+import { CountInput } from "./components/CountInput";
+import { CountText } from "./components/CountText";
 
 export const App = () => {
   // カウントのステート
+  // 第1引数は初期値
   const [count, setCount] = useState<number>(0);
   // テキストのカラー
   const [isTextColorRed, setIsTextColorRed] = useState<boolean>(false);
 
   // カウントを+1する関数
   const countUp = () => {
-    setCount(count + 1);
+    // 現在の値を受け取って新しい値を返す
+    // ステートが変更されると再レンダリングが起こる
+    // 再レンダリングではコンポーネントの呼び出し(計算)が行われる
+    // 前回の結果と差分があればDOMにコミット(反映)
+    setCount((count) => count + 1);
   };
-
-  // カウントを-1するボタンと0にするボタンを作ろう!!
 
   // 副作用(周囲の(変数の)状態を変化させてしまうこと)
   // つまり何らかの処理でDOMが書き換えられ
@@ -23,12 +26,15 @@ export const App = () => {
   // この場合、第二引数の配列に指定した値(count)が変更された時に
   // 実行される
   useEffect(() => {
+    console.log("count val updated");
     setIsTextColorRed(count >= 5 ? true : false);
   }, [count]);
 
   return (
     <div className="mx-auto w-1/3">
+      {/* cssはtailwind */}
       <div className="flex flex-col mt-6 text-center">
+        {/* 子コンポーネントにバケツリレー!! */}
         <CountText count={count} isTextColorRed={isTextColorRed} />
         <CountInput setCount={setCount} />
         <CountUpButton countUp={countUp} />
--- src/components/CountText.tsx
+++ src/components/CountText.tsx
@@ -9,6 +9,7 @@ export const CountText = ({ count, isTextColorRed }: Props) => {
       <p
         className={`text-8xl ${isTextColorRed ? "text-red-600" : "text-black"}`}
       >
+        {/* Propsが更新されると再レンダリングが発生する */}
         {count}
       </p>
     </div>
